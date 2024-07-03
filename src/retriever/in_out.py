1951647f204961792f02fa9b38c60f956ce73353
Timo Luebbing <timo.luebbing@gmail.com>
2024-07-03 18:03:49 +0200

    BM25 + restructure retriever code into multiple files

---
+++ src/retriever/in_out.py
@@ -0,0 +1,57 @@
+import os
+import json
+import csv
+
+from scipy.sparse import csr_matrix
+
+
+def load_corpus_from_json_files(directory_path):
+    corpus = {}
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory_path, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                # Assuming the text content you want is under a key named 'text'.
+                # Adjust the key according to the actual structure of your JSON files.
+                corpus[filename[:-5]] = data['text']
+    return corpus
+
+
+def load_url_mapping_from_csv(csv_file_path):
+    url_mapping = {}
+    
+    with open(csv_file_path, mode='r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        header = reader.fieldnames
+        print("CSV Header:", header)
+        
+        for row in reader:
+            doc_id, url = row[header[0]], row[header[1]]
+            url_mapping[doc_id] = url
+            
+    return url_mapping
+
+
+def convert_csr_to_dict(obj):
+    if isinstance(obj, csr_matrix):
+        # Convert csr_matrix to a dictionary that holds non-zero elements and their indices
+        return {
+            "type": "csr_matrix", 
+            "data": obj.data.tolist(), 
+            "indices": obj.indices.tolist(), 
+            "indptr": obj.indptr.tolist(), 
+            "shape": obj.shape
+        }
+    raise TypeError("Object of type %s is not JSON serializable" % type(obj).__name__)
+
+
+def load_csr_matrix(data):
+    if data['type'] == 'csr_matrix':
+        return csr_matrix(
+            (data['data'], data['indices'], data['indptr']), 
+            shape=data['shape']
+        )
+    else:
+        raise ValueError("JSON does not contain csr_matrix data")
---
+++ src/retriever/main.py
@@ -0,0 +1,90 @@
+import sys
+
+from pathlib import Path
+sys.path.insert(0, Path(__file__).resolve().parents[1])
+print(Path(__file__).resolve().parents[1])
+
+# internal imports:
+from index import Index
+from bm25 import BM25
+from in_out import load_corpus_from_json_files, load_url_mapping_from_csv
+
+def dummy_usage():
+    
+    # Example usage
+    corpus = {
+        'doc1': 'This is a sample document of french bulldogs.',
+        'doc2': 'Another french sample document.',
+        'doc3': 'This french document contains sample words.',
+        'doc4': 'That document contains sample words again.',
+        'doc5': 'This doc consists of example words about french bulldogs.',
+        'doc6': 'This is a test document. Bulldogs are really cute.',
+        'doc7': 'This is a french testing document for french classes',
+        'doc8': 'Bulldogs are really cool. Except the french ones. They are too french.',
+    }
+    index_name = 'test_index'
+    index = Index(corpus)
+    index.initialize_index()
+    index.export_index(index_name)
+    
+    # tfidf ranker class using sklearn fit transform
+    index_path = Path("dat", f"{index_name}.json")
+    bm25 = BM25(index_path)
+    
+    ranked_docs = bm25.rank('french bulldog')
+    print(ranked_docs)
+    
+
+def create_index(corpus, index_name, exist_ok=True):
+    """
+    Create an index for the given corpus and export it to a JSON file.
+
+    Args:
+        corpus (list): The corpus to create the index from.
+        index_name (str): The name of the index.
+        exist_ok (bool, optional): If True, return the path to the existing index file
+            if it already exists. If False, overwrite the existing index file. 
+            Defaults to True.
+
+    Returns:
+        Path: The path to the index file.
+
+    """
+    path_to_index: Path = Path("dat", f"{index_name}.json")
+    if exist_ok and path_to_index.exists():
+        print(f"Index file '{index_name}.json' found. Using pre-computed index")
+        return path_to_index
+    
+    index = Index(corpus)
+    index.initialize_index()
+    index.export_index(index_name)
+    
+    return path_to_index
+    
+
+def main():    
+    
+    # Try on crawled tuebingen data
+    directory_path = "dat/crawler_test_run/crawler/index/"
+    corpus_tue = load_corpus_from_json_files(directory_path)
+    
+    mapping_path = "dat/crawler_test_run/crawler/20240703010421.csv"
+    url_mapping = load_url_mapping_from_csv(mapping_path)
+    
+    # ! Set exist_ok to False if corpus has changed
+    path_to_index = create_index(corpus_tue, "index_tue_v0", exist_ok=True)
+    
+    query = 'Wo kann man gut Wein trinken in Tübingen?'
+    bm25 = BM25(path_to_index)
+    ranked_docs = bm25.rank(query)
+    
+    top5 = ranked_docs[:5]
+    print("Top 5 documents for 'gastronomie in tübingen restaurant':")
+    for doc_id, score in top5:
+        print(f"Document ID: {doc_id}, Score: {score:.4f}, URL: {url_mapping.get(doc_id)}")
+        
+        
+
+if __name__ == "__main__":
+    # dummy_usage()
+    main()
