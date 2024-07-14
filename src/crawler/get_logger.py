9977607ba4e3f375a1d626d21ceb29920530e451
Niclas Lietzow <niclas.lietzow@outlook.com>
2024-07-14 15:37:17 +0200

    fix logger

--- src/crawler/get_logger.py
+++ src/crawler/get_logger.py
@@ -5,7 +5,7 @@ def get_logger(name: str, level: int = logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
+    handler.setLevel(level)
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
